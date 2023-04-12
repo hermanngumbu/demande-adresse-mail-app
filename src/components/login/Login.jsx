a55cc1c022f7959c4564b27baa90f825f3c17c7a
mesch-IT <meschackkapanga@gmail.com>
2023-04-12 20:42:46 +0200

    feat: login component created

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import Navbar from '../navbar/Navbar'
+import './login.css'
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    
+    const submitData = (data) => { 
+          console.log(data)
+    }
   return (
       <>
           <Navbar />
+          <div className='main'>
+              <div className='card'>
+                  <div className="card-body">
+                      <div>
+                          <h1 className='card-title'>Authentification</h1>
+                      </div>
+                      <div>
+                          <form className='form' onSubmit={handleSubmit(submitData)} >
+                              <div className='form-input'>
+                                  <div>
+                                      <label htmlFor="">Votre adresse mail</label>
+                                  </div>
+                                  <div className='form-input-text'>
+                                      <input type="email" {...register('email')} />
+                                      {errors.email && <p>email is required </p> }
+                                  </div>
+                              </div>
+                              <div className='form-input'>
+                                  <div>
+                                      <label htmlFor="">Votre mot de passe</label>
+                                  </div>
+                                  <div className='form-input-text'>
+                                      <input type="password" {...register('password')} />
+                                  </div>
+                              </div>
+                              <div>
+                                  <button type='submit' className='btn-connexion'>SE CONNECTER</button>
+                              </div>
+                          </form>
+                      </div>
+                 </div>
+              </div>
+          </div>
       </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import logo from '../../images/Logo-adn.png'
 import iconAdmin from '../../images/Vector.png'
 import  './navbar.css'
 
 const Navbar = () => {
+    
+    const navigate = useNavigate()
+   const  handleClick = () => { 
+         
+        navigate('/login')
+    }
   return (
       <>
+          
+
           <div className="navbar">
               <div className='left'>
                   <img src={logo} alt="logo adn" />
               </div>
               <div className='right'>
-                  <div>
-                      <h1 className='text-admin' >Se connecter en tant que admin</h1>
+                  <div >
+                      <h1 className='text-admin' onClick={handleClick} >Se connecter en tant que admin</h1>
                   </div>
                   <div>
                       <img src={iconAdmin} alt="icone admin" />
