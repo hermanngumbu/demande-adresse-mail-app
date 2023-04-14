52489728f4d47a7a2f62b394b52872a91f2a8c76
mesch-IT <meschackkapanga@gmail.com>
2023-04-14 03:43:57 +0200

    feat: ajouter une demande

--- src/components/userForm/UserForm.jsx
+++ src/components/userForm/UserForm.jsx
@@ -4,20 +4,62 @@ import Button from 'react-bootstrap/Button';
 import './userForm.css';
 import { Container, FloatingLabel, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
+import { useForm } from "react-hook-form"
+import axios from "axios";
+
 
 function App() {
+
     const [validated, setValidated] = useState(false);
+  const { register, handleSubmit, reset,watch,setValue } = useForm()
+  const prenom = watch('prenom');
+  const nom = watch('nom');
 
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-         setValidated(true);
-      };
+  const generateEmails = () => {
+    const emails = [];
+    if (prenom && nom) {
+      emails.push(`${prenom}.${nom}@example.com`);
+      emails.push(`${prenom[0]}.${nom}@example.com`);
+      emails.push(`${prenom}${nom[0]}@example.com`);
+    }
+    return emails;
+  };
+  if (Object.keys(generateEmails()).length > 0) {
+     setValue('mail1', generateEmails()[0])
+     setValue('mail2', generateEmails()[1])
+     setValue('mail3', generateEmails()[2])
+   }
+  
+  const submitData = (data) => {
+    let body = {
+      name: data.nom,
+      firstname: data.prenom,
+      lastname: data.postnom,
+      fonction: data.fonction,
+      service: data.service,
+      numtel: data.phone,
+      email: data.emailprivate,
+      email_format1: data.mail1,
+      email_format2: data.mail2,
+      email_format3: data.mail3,
 
-    
+    }
+    axios.post("http://localhost:4000/api/demands", body)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => { 
+        console.log(error)
+      })
+    //     const form = event.currentTarget;
+    //     if (form.checkValidity() === false) {
+    //       event.preventDefault();
+    //       event.stopPropagation();
+    //     }
+    // setValidated(true);
+    reset()
+      }
+
 
   return (
     <>
@@ -26,20 +68,20 @@ function App() {
     <p class="h1 text-center">Fiche  de demande d’adresse mail présidence RDC</p>
     </div>
     <div className='form-wrapper'>
-      <Form noValidate validated={validated}>
+        <Form noValidate validated={validated} onSubmit={handleSubmit(submitData)}>
         <h2 style={{ marginBottom: '15px', textAlign: 'center' }}>
         Merci de renseigner toutes vos informations &#9997;
         </h2>
         <Container fluid>
           <Row>
             <Col sm={6} style={{ marginBottom: '10px' }}>
-              <FloatingLabel controlId='firstnamLabel' label='Nom'>
-                <Form.Control type='text' placeholder='Nom' required />
+                <FloatingLabel controlId='firstnamLabel' label='Nom' >
+                  <Form.Control type='text' placeholder='Nom' required {...register('nom',{required:true})} />
               </FloatingLabel>
             </Col>
             <Col sm={6} style={{ marginBottom: '10px' }}>
               <FloatingLabel controlId='lastnameLabel' label='Post-Nom'>
-                <Form.Control type='text' placeholder='Post-Nom' />
+                  <Form.Control type='text' placeholder='Post-Nom' {...register('postnom', { required: true })} />
               </FloatingLabel>
             </Col>
           </Row>
@@ -47,12 +89,12 @@ function App() {
           <Row>
             <Col sm={6} style={{ marginBottom: '10px' }}>
               <FloatingLabel controlId='firstnamLabel' label='Prénom'>
-                <Form.Control type='text' placeholder='Prénom' required />
+                  <Form.Control type='text' placeholder='Prénom' required  {...register('prenom', { required: true })} />
               </FloatingLabel>
             </Col>
             <Col sm={6} style={{ marginBottom: '10px' }}>
               <FloatingLabel controlId='lastnameLabel' label='Téléphone'>
-                <Form.Control type='tel' placeholder='Téléphone' />
+                  <Form.Control type='text' placeholder='Téléphone'  {...register('phone', { required: true })} />
               </FloatingLabel>
             </Col>
           </Row>
@@ -60,7 +102,8 @@ function App() {
           <Row>
             <Form.Group className='mb-3' controlId='formBasicEmail'>
               <FloatingLabel controlId='emailLabel' label='Email privé'>
-                <Form.Control type='email' placeholder='Email privé' required pattern='^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$' />
+                  <Form.Control type='email' placeholder='Email privé'
+                    required pattern='^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'  {...register('emailprivate', { required: true })} />
               </FloatingLabel>
               
             </Form.Group>
@@ -69,11 +112,11 @@ function App() {
           <Row>
             <Form.Group className='mb-3' controlId=''>
               <FloatingLabel controlId='servicelLabel' label='Service'>
-              <select class="form-select" id="floatingSelect">
+                  <select class="form-select" id="floatingSelect"  {...register('service', { required: true })}>
                 <option selected>Selectionner votre service</option>
-                <option value="1">Information Technologie</option>
-                <option value="2">Human ressources</option>
-                <option value="3">Acount management</option>
+                    <option value="Information Technologie">Information Technologie</option>
+                    <option value="Human ressources">Human ressources</option>
+                    <option value="Acount management">Acount management</option>
               </select>
               </FloatingLabel>
               
@@ -82,11 +125,11 @@ function App() {
           <Row>
             <Form.Group className='mb-3' controlId=''>
               <FloatingLabel controlId='fonctionlLabel' label='Fonction'>
-              <select class="form-select" id="floatingSelect">
+                  <select class="form-select" id="floatingSelect"  {...register('fonction', { required: true })}>
                 <option selected>Selectionner votre fonction</option>
-                <option value="1">Fonction 1</option>
-                <option value="2">Fonction 2</option>
-                <option value="3">Fonction 3</option>
+                    <option value="Fonction 1">Fonction 1</option>
+                    <option value="Fonction 2">Fonction 2</option>
+                    <option value="Fonction 3">Fonction 3</option>
               </select>
               </FloatingLabel>
               
@@ -106,7 +149,8 @@ function App() {
             <div class="row mb-3">
             <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">mail 1</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="hermanngumbu@presidence.cd" disabled/>
+                    <input type="email" class="form-control form-control-sm" id="colFormLabelSm"
+                      placeholder="hermanngumbu@presidence.cd" disabled  {...register('mail1')} />
             </div>
             </div>
 
@@ -116,9 +160,10 @@ function App() {
           <Row>
             <Form.Group className='mb-3' controlId=''>
             <div class="row mb-3">
-            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">mail 1</label>
+            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">mail 2</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="hermanngumbu@presidence.cd" disabled/>
+                    <input type="email" class="form-control form-control-sm" id="colFormLabelSm"
+                      placeholder="hermanngumbu@presidence.cd" disabled  {...register('mail2')} />
             </div>
             </div>
 
@@ -128,9 +173,10 @@ function App() {
           <Row>
             <Form.Group className='mb-3' controlId=''>
             <div class="row mb-3">
-            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">mail 1</label>
+            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">mail 3</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="hermanngumbu@presidence.cd" disabled/>
+                    <input type="email" class="form-control form-control-sm" id="colFormLabelSm"
+                      placeholder="hermanngumbu@presidence.cd" disabled  {...register('mail3')} />
             </div>
             </div>
 
@@ -139,7 +185,7 @@ function App() {
           </Row>
           <Row>
              <div class="d-grid gap-2 col-12 mx-auto">
-            <button class="btn btn-primary" type="button" onClick={handleSubmit}>SOUMETTRE &#10004;</button>
+            <button class="btn btn-primary" type="submit">SOUMETTRE &#10004;</button>
             </div>
           </Row>
           
