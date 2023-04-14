0f2ea85c615e827ea29b4463e0504229b7931811
mesch-IT <meschackkapanga@gmail.com>
2023-04-14 11:17:52 +0200

    fix: accepting mail by dntic

--- src/components/dntic/FormDntic.jsx
+++ src/components/dntic/FormDntic.jsx
@@ -1,14 +1,53 @@
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MydModalWithGrid from './detailsModal'
 import './formdntic.css';
 import {Button} from 'react-bootstrap';
+import axios from 'axios';
 
 
 const FormDntic = () => {
-  const [modalShow, setModalShow] = useState(false); 
-  
+  const [modalShow, setModalShow] = useState(false);
+
+
+  const [demands, setDemands] = useState([])
+  // const [isDisabled, setIsDisabled] = useState(false);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/demands/getDemandsWithEmailChoose")
+      .then((response) => {
+        setDemands([...response.data])
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  const handleClick = (event) => {
+    event.target.disabled = true
+  }
+  const allDemands = demands.map((demand) => {
+    return (
+      <div className="principal-carte" key={demand.id}>
+        <div className="carte-dntic">
+          <div>
+            <p>{demand.email_choose}</p>
+
+          </div>
+          <div className='btn-right'>
+            <div>
+              <Button variant="success" onClick={(event) => handleClick( event)}>Approuvé</Button>
+            </div>
+            <div>
+              <Button variant="info" onClick={() => setModalShow(true)}>Détails</Button>{' '}
+              <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  })
   return (
     <>
  
@@ -16,44 +55,9 @@ const FormDntic = () => {
         <div>
           <h1 className='principal-title'>Bienvenu(e) Meschack Kapanga</h1>
         </div>
-        <div className="principal-carte">
-          <div className="carte-dntic">
-            <div>
-              <p>hermanngumbu@gmail.com</p>
-            </div>
-            <div className='btn-right'>
-            <div>
-              <Button variant="success">Approuvé</Button>{' '}
-              </div>
-              <div>
-              <Button variant="danger">Réjetté</Button>{' '}
-              </div>
-              <div>
-              <Button variant="info" onClick={() => setModalShow(true)}>Détails</Button>{' '}
-              <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="principal-carte">
-          <div className="carte-dntic">
-            <div>
-              <p>hermanngumbu@gmail.com</p>
-            </div>
-            <div className='btn-right'>
-              <div>
-                <Button variant="success">Approuvé</Button>{' '}
-              </div>
-              <div>
-              <Button variant="danger">Réjetté</Button>{' '}
-              </div>
-              <div>
-              <Button variant="info" onClick={() => setModalShow(true)}>Détails</Button>{' '}
-              <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
-              </div>
-            </div>
-          </div>
-        </div>
+        {
+          allDemands
+         }
       </div>
     </>
   )
